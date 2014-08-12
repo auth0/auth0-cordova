d5b9f891f7139a1d3ba9d50468c94bff7616b8ae
Martin Gontovnikas <martin@gon.to>
2014-08-12 18:12:23 -0300

    Added cordova sample

---
+++ README.md
@@ -0,0 +1,7 @@
+# Auth0 + Cordova/Phonegap Framework
+
+In this repository, you'll find all the information about integrating Auth0 with Cordova Framework.
+
+To read the documentation, please [click here](https://docs.auth0.com/new/native-platforms/cordova)
+
+Or check out the examples that we have in here in our examples folder. Each of them has a README on how to run them and on how to use them.
---
+++ examples/basic-sample/platforms/ios/www/js/app.js
@@ -0,0 +1,59 @@
+document.addEventListener('deviceready',function() {
+    var widget = new Auth0Widget({
+        // All this properties are set on auth0-variables.js
+        domain: AUTH0_DOMAIN,
+        clientID: AUTH0_CLIENT_ID,
+        callbackURL: AUTH0_CALLBACK_URL,
+        callbackOnLocationHash: true
+    });
+
+    var userProfile;
+
+    $('.btn-login').click(function(e) {
+      e.preventDefault();
+      widget.signin({ popup: true} , null, function(err, profile, token) {
+        if (err) {
+          // Error callback
+          console.log("There was an error");
+          alert("There was an error logging in");
+        } else {
+          // Success calback
+
+          // Save the JWT token.
+          localStorage.setItem('userToken', token);
+
+          // Save the profile
+          userProfile = profile;
+
+          $('.login-box').hide();
+          $('.logged-in-box').show();
+          $('.nickname').text(profile.nickname);
+          $('.nickname').text(profile.name);
+          $('.avatar').attr('src', profile.picture);
+        }
+      });
+    });
+
+    $.ajaxSetup({
+      'beforeSend': function(xhr) {
+        if (localStorage.getItem('userToken')) {
+          xhr.setRequestHeader('Authorization',
+                'Bearer ' + localStorage.getItem('userToken'));
+        }
+      }
+    });
+
+    $('.btn-api').click(function(e) {
+      // Just call your API here. The header will be sent
+      $.ajax({
+        url: 'http://auth0-nodejsapi-sample.herokuapp.com/secured/ping',
+        method: 'GET'
+      }).then(function(data, textStatus, jqXHR) {
+        alert("The request to the secured enpoint was successfull");
+      }, function() {
+        alert("You need to download the server seed and start it to call this API");
+      });
+    });
+
+
+}, false);
