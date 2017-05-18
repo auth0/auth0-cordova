1f2ed809259cb7f2f86657685354ef6da7f2e17d
Mark de Cates <decates@gmail.com>
2017-05-18 11:36:46 +0100

    Handle InAppBrowser exit event correctly

--- src/session.js
+++ src/session.js
@@ -1,18 +1,23 @@
 function Session() {}
 
 Session.clean = function () {
-  Session.current = function () { return false; };
+    Session.current = function () { return false; };
+    Session.isClosing = false;
+};
+
+Session.closing = function () {
+    Session.isClosing = true;
 };
 
 Session.start = function (handler) {
-  Session.current(new Error('Only one instance of auth can happen at a time'));
-  Session.current = handler;
+    Session.current(new Error('Only one instance of auth can happen at a time'));
+    Session.current = handler;
 };
 
 Session.onRedirectUri = function (url) {
-  if (Session.current(null, url)) {
-    Session.clean();
-  }
+    if (Session.current(null, url)) {
+        Session.clean();
+    }
 };
 
 module.exports = Session;
