0f105dd19ac48e339783ff963f3f8df47906cdb6
Ryan Chenkie <ryanchenkie@gmail.com>
2017-07-10 11:35:34 -0400

    deprecate Cordova sample

--- example/cordova/env.example.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    domain: 'Your auth0 domain',
-    clientID: 'Your Client ID',
-    packageIdentifier: 'Your Package Identifier com.example.capp',
-    audience: 'Optional argument, should be your api identifier in https://manage.auth0.com/#/apis'
-}
