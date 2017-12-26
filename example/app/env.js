be9a509727a563a66d40cbb481178dbbd3518f5e
Abhishek Hingnikar <abhishek@goph.me>
2017-12-26 13:33:11 -0500

    Add WKWebview Demo

--- example/app/env.js
+++ example/app/env.js
@@ -1,5 +1,8 @@
 module.exports = {
   domain: 'auth0conciergedemos.auth0.com',
   clientID: 'qXZtRFZ3SDYGhyjwIZi1B1nWofCNlQ43',
-  packageIdentifier: 'com.auth0.example.cordova'
+  // Dynamically pass the correct controller
+  packageIdentifier: (window.webkit && window.webkit.messageHandlers)
+    ? 'com.auth0samplescordova.wkwebview'
+    : 'com.auth0samplescordova.simple'
 };
