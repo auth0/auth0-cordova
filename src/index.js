32533cc486cc3a7660434c7d8d36e5599d2a4c6b
Jyotishka Biswas <jyotishka@courbanize.com>
2019-10-02 16:26:23 -0400

    Register session.start handler before opening agent

--- src/index.js
+++ src/index.js
@@ -92,6 +92,47 @@ CordovaAuth.prototype.authorize = function (parameters, callback) {
 
     var url = client.buildAuthorizeUrl(params);
 
+    session.start(function (sessionError, redirectUrl) {
+      if (sessionError != null) {
+        callback(sessionError);
+        return true;
+      }
+      if (redirectUrl.indexOf(redirectUri) === -1) {
+        return false;
+      }
+      if (!redirectUrl || typeof redirectUrl !== 'string') {
+        callback(new Error('url must be a string'));
+        return true;
+      }
+      var response = parse(redirectUrl, true).query;
+      if (response.error) {
+        callback(new Error(response.error_description || response.error));
+        return true;
+      }
+      var responseState = response.state;
+      if (responseState !== requestState) {
+        callback(new Error('Response state does not match expected state'));
+        return true;
+      }
+      var code = response.code;
+      var verifier = keys.codeVerifier;
+      agent.close();
+
+      client.oauthToken({
+        code_verifier: verifier,
+        grantType: 'authorization_code',
+        redirectUri: redirectUri,
+        code: code
+      }, function (exchangeError, exchangeResult) {
+        if (exchangeError) {
+          return callback(exchangeError);
+        }
+        return callback(null, exchangeResult);
+      });
+
+      return true;
+    });
+
     agent.open(url, function (error, result) {
       if (error != null) {
         session.clean();
@@ -119,52 +160,6 @@ CordovaAuth.prototype.authorize = function (parameters, callback) {
         // Ignore any other events.
         return;
       }
-
-      session.start(function (sessionError, redirectUrl) {
-        if (sessionError != null) {
-          callback(sessionError);
-          return true;
-        }
-
-        if (redirectUrl.indexOf(redirectUri) === -1) {
-          return false;
-        }
-
-        if (!redirectUrl || typeof redirectUrl !== 'string') {
-          callback(new Error('url must be a string'));
-          return true;
-        }
-
-        var response = parse(redirectUrl, true).query;
-        if (response.error) {
-          callback(new Error(response.error_description || response.error));
-          return true;
-        }
-
-        var responseState = response.state;
-        if (responseState !== requestState) {
-          callback(new Error('Response state does not match expected state'));
-          return true;
-        }
-
-        var code = response.code;
-        var verifier = keys.codeVerifier;
-        agent.close();
-
-        client.oauthToken({
-          code_verifier: verifier,
-          grantType: 'authorization_code',
-          redirectUri: redirectUri,
-          code: code
-        }, function (exchangeError, exchangeResult) {
-          if (exchangeError) {
-            return callback(exchangeError);
-          }
-          return callback(null, exchangeResult);
-        });
-
-        return true;
-      });
     });
   });
 };
