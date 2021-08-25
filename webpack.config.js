c10a44aea9072cfa4f5e1c70caaeb68730345ae1
Steve Hobbs <steve.hobbs@auth0.com>
2021-08-25 16:41:42 +0100

    Dependency update (#177)
    * Ignore .DS_Store
    
    * Dependency update
    
    * Update dependencies: webpack (4) and auth0.js 9.16.3
    
    * Use newer docker image for build
    
    * Dependency update

--- webpack.config.js
+++ webpack.config.js
@@ -2,27 +2,32 @@ const path = require('path');
 const webpack = require('webpack');
 
 const config = {
-
   context: __dirname,
-
+  mode: 'production',
   entry: {
-    'index.js':'./src/index.js',
+    'index.js': './src/index.js',
   },
 
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: '[name]',
     libraryTarget: 'umd',
-    library: 'PKCEClient'
+    library: 'PKCEClient',
+  },
+
+  resolve: {
+    alias: {
+      'bn.js': path.join(__dirname, 'node_modules/bn.js/lib/bn.js'),
+    },
   },
 
   devtool: 'source-map',
 
   plugins: [
     new webpack.ProgressPlugin((prog) => {
-        if(prog === 0) console.log("[webpack]: Bundle is now invalid.");
-        if(prog === 1) console.log("[webpack]: Bundle is now valid.");
-    })
-  ]
-}
+      if (prog === 0) console.log('[webpack]: Bundle is now invalid.');
+      if (prog === 1) console.log('[webpack]: Bundle is now valid.');
+    }),
+  ],
+};
 module.exports = config;
